a26bb039a399d430f6f98b8b26a04eec2c003865
douni05 <douni05@gmail.com>
2026-03-23 14:24:50 +0900

    🎉INIT:프로젝트 초기설정

---
+++ android/app/src/main/kotlin/com/jimmy/reborn/reborn_fe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.jimmy.reborn.reborn_fe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// Re:Born 브랜드 컬러
+class AppColors {
+  static const Color mainGreen = Color(0xFF81A176); // 메인 그린
+  static const Color pointGreen = Color(0xFF143424); // 포인트 딥그린
+  static const Color backgroundWhite = Color(0xFFFAFCFC); // 배경색
+  static const Color textBlack = Color(0xFF212121); //텍스트 색
+}
